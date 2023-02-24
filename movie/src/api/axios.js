67ab17bc423b7e44f9da51667abd28d4f4585460
leejy001 <greatness007@naver.com>
2023-02-24 14:32:06 +0900

    컴포넌트 구조 및 movie api 설정

---
+++ movie/src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "852cdaf61c9c34aa2a81afec92cc29b0",
+    lanquage: "ko-KR"
+  }
+});
+
+export default instance;
