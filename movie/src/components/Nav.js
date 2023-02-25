2e7e69729d41442dfbaa70d9f5cd5f2f3072895f
leejy001 <greatness007@naver.com>
2023-02-25 23:26:50 +0900

    검색, 검색 결과 페이지 추가, 라우터 추가

--- movie/src/components/Nav.js
+++ movie/src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 function Nav() {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -17,6 +20,12 @@ function Nav() {
       window.removeEventListener("scroll", () => {});
     };
   }, []);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img
@@ -24,6 +33,13 @@ function Nav() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
         className="nav__logo"
         onClick={() => window.location.reload()}
+      />
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
       />
       <img
         alt="User logged"
