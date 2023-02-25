6f0146d4bb7d88c04db198ccb5510822d2a6aef4
leejy001 <greatness007@naver.com>
2023-02-26 00:08:14 +0900

    메인페이지 슬라이드 swiper 적용

--- movie/src/components/Nav.js
+++ movie/src/components/Nav.js
@@ -32,7 +32,7 @@ function Nav() {
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
         className="nav__logo"
-        onClick={() => window.location.reload()}
+        onClick={() => (window.location.href = "/")}
       />
       <input
         value={searchValue}
